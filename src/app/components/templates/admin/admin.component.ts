d05f3dda7735b4c37e2408521cdb3f0e3acc12d7
huynhlehoangduc <huynhlehoangduc@gmail.com>
2022-10-18 18:13:51 +0700

    Add topbar menu

--- src/app/components/templates/admin/admin.component.ts
+++ src/app/components/templates/admin/admin.component.ts
@@ -4,13 +4,14 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatMenuModule} from "@angular/material/menu";
 
 @Component({
   standalone: true,
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
-  imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule]
+    imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatMenuModule]
 })
 export class AdminComponent implements OnInit {
 
